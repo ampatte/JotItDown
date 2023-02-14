48af8c81ec7a45a227e40ab0e3f7a15a1edc0aeb
ampatte <ampatterson1@hotmail.com>
2023-02-14 07:53:05 -0500

    everything is working again!

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,20 @@
 const router = require('express').Router();
 const fs = require('fs');
-const path = require('path');
 // Helper method for generating unique ids
 const { v4: uuidv4 } = require('uuid');
 uuidv4();
-
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 
 //* `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
-router.get('/notes', (req, res) => 
-  fs.readFile('db/db.json', 'utf-8',(err, data) => {
+router.get('/notes', (req, res) => {
+  readFromFile('./db/db.json', 'utf-8',(err, data) => {
     return err ? console.log(err) : res.json(JSON.parse(data))
-  })
-);
+});
+});
 
 //* `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you). 
 router.post('/notes', (req, res) => {
-  console.info(`${req.method} request received to save a note`);
+  console.log(req.body, `request received to save a note`);
   
   const { title, text} = req.body;
   
@@ -27,27 +25,27 @@ router.post('/notes', (req, res) => {
   id: uuidv4(),
   };
   
-  readAndAppend(newNote, '/db/db.json');
+  readAndAppend(newNote, './db/db.json');
   res.json(`Note added successfully 🚀`);
 } else {
   res.err('Error in saving note');
 }});
 
 //DELETE /api/notes/:id` should receive a query parameter that contains the id of a note to delete. To delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
-router.delete('notes/:id', async (req, res) => {
-  readFromFile('*', './db/db.json');
-  console.info(`${req.method} request received to delete a note`);
-  try {
-    const {id} =  note.destroy({
-      where: {
-        id: req.params.id
-       }// await deleteNote(id, note),
-    });
+// router.delete('notes/:id', async (req, res) => {
+//   readFromFile('*', './db/db.json');
+//   console.info(`${req.method} request received to delete a note`);
+//   try {
+//     const {id} =  note.destroy({
+//       where: {
+//         id: req.params.id
+//        }// await deleteNote(id, note),
+//     });
 
-    res.status(200).json(noteId);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.status(200).json(noteId);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,34 +1,21 @@
-const router = require('express').Router();
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 const apiRoutes = require('./routes/apiRoutes');
-//const htmlRoutes = require('./routes/htmlRoutes');
-// Helper method for generating unique ids
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const app = express();
 
 const PORT = process.env.PORT || 3001;
-const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //serves static files from the '/public' folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
-
-
+app.use('/public', express.static( 'public'));
 
 app.use('/api', apiRoutes);
-
-// GET Route for submitting notes
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-  );
-
-  // GET Route for homepage
-  app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-  );
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
